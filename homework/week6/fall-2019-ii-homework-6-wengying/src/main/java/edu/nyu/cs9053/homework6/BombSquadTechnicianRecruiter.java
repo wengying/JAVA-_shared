b529fef879617548dcefb6822873f9cd195657d1
wengying <47150454+wengying@users.noreply.github.com>
2019-10-10 00:50:30 -0400

    Add files via upload

---
+++ homework/week6/fall-2019-ii-homework-6-wengying/src/main/java/edu/nyu/cs9053/homework6/BombSquadTechnicianRecruiter.java
@@ -0,0 +1,14 @@
+package edu.nyu.cs9053.homework6;
+
+/**
+ * User: blangel
+ * Date: 10/11/17
+ * Time: 8:03 AM
+ */
+public class BombSquadTechnicianRecruiter {
+
+    public static BombSquadTechnician recruit() {
+        return null;
+    }
+
+}
