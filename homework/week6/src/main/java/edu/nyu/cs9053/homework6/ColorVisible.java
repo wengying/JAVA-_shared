1871b39e509ab96643e648b4d18904b65bd42909
wengying <ww1419@nyu.edu>
2019-10-31 10:37:23 -0400

    add week7 resources

---
+++ homework/week6/src/main/java/edu/nyu/cs9053/homework6/ColorVisible.java
@@ -0,0 +1,13 @@
+package edu.nyu.cs9053.homework6;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+/**
+ * User: blangel
+ * Date: 10/11/17
+ * Time: 8:43 AM
+ */
+@Target(ElementType.TYPE)
+public @interface ColorVisible {
+}
